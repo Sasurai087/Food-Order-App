4eb08ccbc2cce570b53976648527439c389edd83
Sasurai087 <johsako087@gmail.com>
2021-10-22 19:23:33 -0700

    add cart via React portal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartVisible, setCartVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartVisible(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartVisible(false);
+  };
+
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
