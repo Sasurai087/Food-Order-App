4935400167a9ba24f5c3611a5d32cac5dfe253cf
Sasurai087 <johsako087@gmail.com>
2021-10-23 12:25:14 -0700

    add cart animation and remove item functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [bump, setBump] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
+
+  //object destructuring to pull just items out of cartCtx - now useEffect only fires when there's a change in cart items
+  const { items } = cartCtx;
+  const btnClasses = `${classes.button} ${bump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBump(true);
+    const timer = setTimeout(() => {
+      setBump(false);
+    }, 300);
+
+    //cleanup timer
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
